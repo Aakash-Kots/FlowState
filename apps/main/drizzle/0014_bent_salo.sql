9219ea205e17f91a2292ccc08721cb36b4b96667
Aakash Kothari <kots.aakash@gmail.com>
2026-07-16 18:54:17 -0700

    chore(db): add migration 0014_bent_salo

---
+++ apps/main/drizzle/0014_bent_salo.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `usage_events` ADD `workspace_name` text;--> statement-breakpoint
+ALTER TABLE `usage_events` ADD `branch` text;--> statement-breakpoint
+ALTER TABLE `usage_events` ADD `project_id` text;--> statement-breakpoint
+ALTER TABLE `usage_events` ADD `project_name` text;
